f8d6dc15ca4f4c0df4e63d381cab869e6904364a
AndrewFromMelbourne <arduncan@bigpond.com>
2015-01-27 23:29:03 +1100

    tabs -> spaces

--- webcam/webcam.c
+++ webcam/webcam.c
@@ -63,17 +63,17 @@
 
 typedef struct
 {
-	size_t length;
-	uint8_t *buffer;
+    size_t length;
+    uint8_t *buffer;
 } VIDEO_BUFFER_T;
 
 //-------------------------------------------------------------------------
 
 typedef struct
 {
-	uint8_t y;
-	uint8_t u;
-	uint8_t v;
+    uint8_t y;
+    uint8_t u;
+    uint8_t v;
 } YUV8_T;
 
 //-------------------------------------------------------------------------
@@ -94,10 +94,10 @@ printUsage(
     fprintf(fp, "    --fps <fps> - set desired frames per second");
     fprintf(fp, " (default %d frames per second)\n", DEFAULT_FPS);
     fprintf(fp, "    --pidfile <pidfile> - create and lock PID file (if being run as a daemon)\n");
-	fprintf(fp, "    --width <width> - set video width");
-	fprintf(fp, " (default %d)\n", DEFAULT_WIDTH);
-	fprintf(fp, "    --height <height> - set video height");
-	fprintf(fp, " (default %d)\n", DEFAULT_HEIGHT);
+    fprintf(fp, "    --width <width> - set video width");
+    fprintf(fp, " (default %d)\n", DEFAULT_WIDTH);
+    fprintf(fp, "    --height <height> - set video height");
+    fprintf(fp, " (default %d)\n", DEFAULT_HEIGHT);
     fprintf(fp, "    --help - print usage and exit\n");
     fprintf(fp, "\n");
 }
@@ -122,140 +122,140 @@ signalHandler(
 
 uint8_t
 clip255(
-	int16_t value)
+    int16_t value)
 {
-	if (value < 0)
-	{
-		value = 0;
-	}
-	else if (value > 255)
-	{
-		value = 255;
-	}
+    if (value < 0)
+    {
+        value = 0;
+    }
+    else if (value > 255)
+    {
+        value = 255;
+    }
 
-	return value;
+    return value;
 }
 
 //-------------------------------------------------------------------------
 
 void
 yuvToRgb(
-	const YUV8_T *yuv,
-	RGB8_T *rgb)
+    const YUV8_T *yuv,
+    RGB8_T *rgb)
 {
-	int16_t c = yuv->y - 16;
-	int16_t d = yuv->u - 128;
-	int16_t e = yuv->v - 128;
+    int16_t c = yuv->y - 16;
+    int16_t d = yuv->u - 128;
+    int16_t e = yuv->v - 128;
 
-	rgb->red = clip255((298 * c + 409 * e + 128) >> 8);
-	rgb->green = clip255((298 * c - 100 * d - 208 * e + 128) >> 8);
-	rgb->blue = clip255((298 * c + 516 * d + 128) >> 8);
+    rgb->red = clip255((298 * c + 409 * e + 128) >> 8);
+    rgb->green = clip255((298 * c - 100 * d - 208 * e + 128) >> 8);
+    rgb->blue = clip255((298 * c + 516 * d + 128) >> 8);
 }
 
 //-------------------------------------------------------------------------
 
 bool
 initVideo(
-	bool isDaemon,
-	const char *program,
-	int fd,
-	int *width,
-	int *height, 
-	int fps)
+    bool isDaemon,
+    const char *program,
+    int fd,
+    int *width,
+    int *height, 
+    int fps)
 {
-	//---------------------------------------------------------------------
+    //---------------------------------------------------------------------
 
-	struct v4l2_capability cap;
+    struct v4l2_capability cap;
 
-	if (ioctl(fd, VIDIOC_QUERYCAP, &cap) == -1)
-	{
+    if (ioctl(fd, VIDIOC_QUERYCAP, &cap) == -1)
+    {
         perrorLog(isDaemon,
                   program,
                  "could not query V4L2 device");
 
-		return false;
-	}
+        return false;
+    }
 
-	if ((cap.capabilities & V4L2_CAP_VIDEO_CAPTURE) == 0)
-	{
+    if ((cap.capabilities & V4L2_CAP_VIDEO_CAPTURE) == 0)
+    {
         messageLog(isDaemon,
                    program,
                    LOG_ERR,
                    "no video capture device found");
 
-		return false;
-	}
+        return false;
+    }
 
-	if ((cap.capabilities & V4L2_CAP_STREAMING) == 0)
-	{
+    if ((cap.capabilities & V4L2_CAP_STREAMING) == 0)
+    {
         messageLog(isDaemon,
                    program,
                    LOG_ERR,
                    "no video streaming device found");
 
-		return false;
-	}
+        return false;
+    }
 
-	//---------------------------------------------------------------------
+    //---------------------------------------------------------------------
 
-	struct v4l2_format fmt;
+    struct v4l2_format fmt;
 
-	memset(&fmt, 0, sizeof(fmt));
+    memset(&fmt, 0, sizeof(fmt));
 
-	fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-	fmt.fmt.pix.width = *width;
-	fmt.fmt.pix.height = *height;
-	fmt.fmt.pix.field = V4L2_FIELD_INTERLACED;
-	fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
+    fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    fmt.fmt.pix.width = *width;
+    fmt.fmt.pix.height = *height;
+    fmt.fmt.pix.field = V4L2_FIELD_INTERLACED;
+    fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
 
-	if (ioctl(fd, VIDIOC_S_FMT, &fmt) == -1)
-	{
+    if (ioctl(fd, VIDIOC_S_FMT, &fmt) == -1)
+    {
         perrorLog(isDaemon,
                   program,
                  "could not get required format from video device");
 
-		return false;
-	}
+        return false;
+    }
 
-	if ((fmt.fmt.pix.width != *width) || (fmt.fmt.pix.height != *height))
-	{
-		
-		messageLog(
-			isDaemon,
-			program,
-			LOG_INFO,
-			"video dimension %dx%d -> %dx%d",
-			*width,
-			*height,
-			fmt.fmt.pix.width,
-			fmt.fmt.pix.height);
+    if ((fmt.fmt.pix.width != *width) || (fmt.fmt.pix.height != *height))
+    {
+        
+        messageLog(
+            isDaemon,
+            program,
+            LOG_INFO,
+            "video dimension %dx%d -> %dx%d",
+            *width,
+            *height,
+            fmt.fmt.pix.width,
+            fmt.fmt.pix.height);
 
-		*width = fmt.fmt.pix.width;
-		*height = fmt.fmt.pix.height;
-	}
+        *width = fmt.fmt.pix.width;
+        *height = fmt.fmt.pix.height;
+    }
 
-	//---------------------------------------------------------------------
+    //---------------------------------------------------------------------
 
-	if (fps > 0)
-	{
-		struct v4l2_streamparm streamparm;
+    if (fps > 0)
+    {
+        struct v4l2_streamparm streamparm;
 
-		memset(&streamparm, 0, sizeof(streamparm));
+        memset(&streamparm, 0, sizeof(streamparm));
 
-		streamparm.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-		streamparm.parm.capture.timeperframe.numerator = 1;
-		streamparm.parm.capture.timeperframe.denominator = fps;
+        streamparm.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        streamparm.parm.capture.timeperframe.numerator = 1;
+        streamparm.parm.capture.timeperframe.denominator = fps;
 
-		if (ioctl(fd, VIDIOC_S_PARM, &streamparm) == -1)
-		{
-			messageLog(isDaemon,
-					   program,
-					   LOG_ERR,
-					   "unable to set requested fps (%d)", fps);
-		}
-	}
+        if (ioctl(fd, VIDIOC_S_PARM, &streamparm) == -1)
+        {
+            messageLog(isDaemon,
+                       program,
+                       LOG_ERR,
+                       "unable to set requested fps (%d)", fps);
+        }
+    }
 
-	return true;
+    return true;
 }
 
 //-------------------------------------------------------------------------
@@ -267,9 +267,9 @@ main(
 {
     const char *program = basename(argv[0]);
 
-	int width = DEFAULT_WIDTH;
-	int height = DEFAULT_HEIGHT;
-	int fps = DEFAULT_FPS;
+    int width = DEFAULT_WIDTH;
+    int height = DEFAULT_HEIGHT;
+    int fps = DEFAULT_FPS;
     suseconds_t frameDuration =  1000000 / fps;
     bool isDaemon =  false;
     char *pidfile = NULL;
@@ -307,10 +307,10 @@ main(
             {
                 frameDuration = 1000000 / fps;
             }
-			else
-			{
-				fps = 1000000 / frameDuration;
-			}
+            else
+            {
+                fps = 1000000 / frameDuration;
+            }
 
             break;
 
@@ -466,9 +466,9 @@ main(
 
     //---------------------------------------------------------------------
 
-	if (initVideo(isDaemon, program, vfd, &width, &height, fps) == false)
-	{
-		close(vfd);
+    if (initVideo(isDaemon, program, vfd, &width, &height, fps) == false)
+    {
+        close(vfd);
 
         if (pfh)
         {
@@ -476,15 +476,15 @@ main(
         }
 
         exit(EXIT_FAILURE);
-	}
+    }
 
-	//---------------------------------------------------------------------
+    //---------------------------------------------------------------------
 
-	IMAGE_T image;
+    IMAGE_T image;
 
-	if (initImage(&image, width, height, false) == false)
-	{
-		close(vfd);
+    if (initImage(&image, width, height, false) == false)
+    {
+        close(vfd);
 
         if (pfh)
         {
@@ -492,171 +492,171 @@ main(
         }
 
         exit(EXIT_FAILURE);
-	}
-
-	//---------------------------------------------------------------------
-
-	struct v4l2_requestbuffers reqbuffers;
-
-	memset(&reqbuffers, 0, sizeof(reqbuffers));
-
-	reqbuffers.count = NUMBER_OF_BUFFERS_TO_REQUEST;
-	reqbuffers.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-	reqbuffers.memory = V4L2_MEMORY_MMAP;
-
-	if (ioctl(vfd, VIDIOC_REQBUFS, &reqbuffers) == -1)
-	{
-		if (errno == EINVAL)
-		{
-			messageLog(isDaemon,
-					   program,
-					   LOG_ERR,
-					   "video device does not support memory mapping");
-		}
-		else
-		{
-			perrorLog(isDaemon, program, "could not request video buffers");
-		}
-
-		close(vfd);
-
-		if (pfh)
-		{
-			pidfile_remove(pfh);
-		}
-
-		exit(EXIT_FAILURE);
-	}
-
-	if (reqbuffers.count < 2)
-	{
-		messageLog(isDaemon,
-				   program,
-				   LOG_ERR,
-				   "insufficient buffer memory on video device");
-		close(vfd);
-
-		if (pfh)
-		{
-			pidfile_remove(pfh);
-		}
-
-		exit(EXIT_FAILURE);
-	}
-
-	size_t numberOfVideoBuffers = reqbuffers.count;
-	VIDEO_BUFFER_T *videoBuffers = malloc(numberOfVideoBuffers *
-										  sizeof(VIDEO_BUFFER_T));
-
-	size_t bufferIndex = 0;
-
-	for (bufferIndex = 0 ;
-		 bufferIndex < numberOfVideoBuffers ;
-		 bufferIndex++)
-	{
-		struct v4l2_buffer buffer;
-
-		memset(&buffer, 0, sizeof(buffer));
-
-		buffer.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-		buffer.memory = V4L2_MEMORY_MMAP;
-		buffer.index = bufferIndex;
-
-		if (ioctl(vfd, VIDIOC_QUERYBUF, &buffer) == -1)
-		{
-			perrorLog(
-				isDaemon,
-				program,
-				"could not map video buffers");
-
-			close(vfd);
-
-			if (pfh)
-			{
-				pidfile_remove(pfh);
-			}
-
-			exit(EXIT_FAILURE);
-		}
-
-		videoBuffers[bufferIndex].length = buffer.length;
-		videoBuffers[bufferIndex].buffer = mmap(NULL,
-												buffer.length,
-												PROT_READ | PROT_WRITE,
-												MAP_SHARED,
-												vfd,
-												buffer.m.offset);
-
-		if (videoBuffers[bufferIndex].buffer == MAP_FAILED)
-		{
-			perrorLog(
-				isDaemon,
-				program,
-				"video buffer memory map failed");
-
-			close(vfd);
-
-			if (pfh)
-			{
-				pidfile_remove(pfh);
-			}
-
-			exit(EXIT_FAILURE);
-		}
-	}
+    }
 
     //---------------------------------------------------------------------
 
-	for (bufferIndex = 0 ;
-		 bufferIndex < numberOfVideoBuffers ;
-		 bufferIndex++)
-	{
-		struct v4l2_buffer buffer;
+    struct v4l2_requestbuffers reqbuffers;
 
-		memset(&buffer, 0, sizeof(buffer));
+    memset(&reqbuffers, 0, sizeof(reqbuffers));
 
-		buffer.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-		buffer.memory = V4L2_MEMORY_MMAP;
-		buffer.index = bufferIndex;
+    reqbuffers.count = NUMBER_OF_BUFFERS_TO_REQUEST;
+    reqbuffers.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    reqbuffers.memory = V4L2_MEMORY_MMAP;
 
-		if (ioctl(vfd, VIDIOC_QBUF, &buffer) == -1)
-		{
-			perrorLog(
-				isDaemon,
-				program,
-				"could not enqueue video buffers");
+    if (ioctl(vfd, VIDIOC_REQBUFS, &reqbuffers) == -1)
+    {
+        if (errno == EINVAL)
+        {
+            messageLog(isDaemon,
+                       program,
+                       LOG_ERR,
+                       "video device does not support memory mapping");
+        }
+        else
+        {
+            perrorLog(isDaemon, program, "could not request video buffers");
+        }
 
-			close(vfd);
+        close(vfd);
 
-			if (pfh)
-			{
-				pidfile_remove(pfh);
-			}
+        if (pfh)
+        {
+            pidfile_remove(pfh);
+        }
 
-			exit(EXIT_FAILURE);
-		}
-	}
+        exit(EXIT_FAILURE);
+    }
+
+    if (reqbuffers.count < 2)
+    {
+        messageLog(isDaemon,
+                   program,
+                   LOG_ERR,
+                   "insufficient buffer memory on video device");
+        close(vfd);
+
+        if (pfh)
+        {
+            pidfile_remove(pfh);
+        }
+
+        exit(EXIT_FAILURE);
+    }
+
+    size_t numberOfVideoBuffers = reqbuffers.count;
+    VIDEO_BUFFER_T *videoBuffers = malloc(numberOfVideoBuffers *
+                                          sizeof(VIDEO_BUFFER_T));
+
+    size_t bufferIndex = 0;
+
+    for (bufferIndex = 0 ;
+         bufferIndex < numberOfVideoBuffers ;
+         bufferIndex++)
+    {
+        struct v4l2_buffer buffer;
+
+        memset(&buffer, 0, sizeof(buffer));
+
+        buffer.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buffer.memory = V4L2_MEMORY_MMAP;
+        buffer.index = bufferIndex;
+
+        if (ioctl(vfd, VIDIOC_QUERYBUF, &buffer) == -1)
+        {
+            perrorLog(
+                isDaemon,
+                program,
+                "could not map video buffers");
+
+            close(vfd);
+
+            if (pfh)
+            {
+                pidfile_remove(pfh);
+            }
+
+            exit(EXIT_FAILURE);
+        }
+
+        videoBuffers[bufferIndex].length = buffer.length;
+        videoBuffers[bufferIndex].buffer = mmap(NULL,
+                                                buffer.length,
+                                                PROT_READ | PROT_WRITE,
+                                                MAP_SHARED,
+                                                vfd,
+                                                buffer.m.offset);
+
+        if (videoBuffers[bufferIndex].buffer == MAP_FAILED)
+        {
+            perrorLog(
+                isDaemon,
+                program,
+                "video buffer memory map failed");
+
+            close(vfd);
+
+            if (pfh)
+            {
+                pidfile_remove(pfh);
+            }
+
+            exit(EXIT_FAILURE);
+        }
+    }
 
     //---------------------------------------------------------------------
 
-	enum v4l2_buf_type buf_type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    for (bufferIndex = 0 ;
+         bufferIndex < numberOfVideoBuffers ;
+         bufferIndex++)
+    {
+        struct v4l2_buffer buffer;
 
-	if (ioctl(vfd, VIDIOC_STREAMON, &buf_type) == -1)
-	{
-		perrorLog(
-			isDaemon,
-			program,
-			"could not start video capture");
+        memset(&buffer, 0, sizeof(buffer));
 
-		close(vfd);
+        buffer.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buffer.memory = V4L2_MEMORY_MMAP;
+        buffer.index = bufferIndex;
 
-		if (pfh)
-		{
-			pidfile_remove(pfh);
-		}
+        if (ioctl(vfd, VIDIOC_QBUF, &buffer) == -1)
+        {
+            perrorLog(
+                isDaemon,
+                program,
+                "could not enqueue video buffers");
 
-		exit(EXIT_FAILURE);
-	}
+            close(vfd);
+
+            if (pfh)
+            {
+                pidfile_remove(pfh);
+            }
+
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    //---------------------------------------------------------------------
+
+    enum v4l2_buf_type buf_type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+
+    if (ioctl(vfd, VIDIOC_STREAMON, &buf_type) == -1)
+    {
+        perrorLog(
+            isDaemon,
+            program,
+            "could not start video capture");
+
+        close(vfd);
+
+        if (pfh)
+        {
+            pidfile_remove(pfh);
+        }
+
+        exit(EXIT_FAILURE);
+    }
 
     //---------------------------------------------------------------------
 
@@ -672,80 +672,80 @@ main(
 
         //-----------------------------------------------------------------
 
-		struct v4l2_buffer buffer;
+        struct v4l2_buffer buffer;
 
-		memset(&buffer, 0, sizeof(buffer));
+        memset(&buffer, 0, sizeof(buffer));
 
-		buffer.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-		buffer.memory = V4L2_MEMORY_MMAP;
+        buffer.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buffer.memory = V4L2_MEMORY_MMAP;
 
-		if (ioctl(vfd, VIDIOC_DQBUF, &buffer) == -1)
-		{
-			perrorLog(
-				isDaemon,
-				program,
-				"could not dequeue video buffers");
+        if (ioctl(vfd, VIDIOC_DQBUF, &buffer) == -1)
+        {
+            perrorLog(
+                isDaemon,
+                program,
+                "could not dequeue video buffers");
 
-			close(vfd);
+            close(vfd);
 
-			if (pfh)
-			{
-				pidfile_remove(pfh);
-			}
+            if (pfh)
+            {
+                pidfile_remove(pfh);
+            }
 
-			exit(EXIT_FAILURE);
-		}
+            exit(EXIT_FAILURE);
+        }
 
-		uint8_t *yuyv = videoBuffers[buffer.index].buffer;
+        uint8_t *yuyv = videoBuffers[buffer.index].buffer;
 
-		int y;
-		for (y = 0 ; y < height ; y++)
-		{
-			int x;
-			for (x = 0 ; x < width ; x++)
-			{
-				size_t offset =  2 * (x + (y * width));
+        int y;
+        for (y = 0 ; y < height ; y++)
+        {
+            int x;
+            for (x = 0 ; x < width ; x++)
+            {
+                size_t offset =  2 * (x + (y * width));
 
-				RGB8_T rgb;
-				YUV8_T yuv;
+                RGB8_T rgb;
+                YUV8_T yuv;
 
-				yuv.y = yuyv[offset];
+                yuv.y = yuyv[offset];
 
-				if (offset % 4)
-				{
-					yuv.u = yuyv[offset - 1];
-					yuv.v = yuyv[offset + 1];
-				}
-				else
-				{
-					yuv.u = yuyv[offset + 1];
-					yuv.v = yuyv[offset + 3];
-				}
+                if (offset % 4)
+                {
+                    yuv.u = yuyv[offset - 1];
+                    yuv.v = yuyv[offset + 1];
+                }
+                else
+                {
+                    yuv.u = yuyv[offset + 1];
+                    yuv.v = yuyv[offset + 3];
+                }
 
-				yuvToRgb(&yuv, &rgb);
+                yuvToRgb(&yuv, &rgb);
 
-				setPixelRGB(&image, x, y, &rgb);
-			}
-		}
+                setPixelRGB(&image, x, y, &rgb);
+            }
+        }
 
-		putImageLcd(&lcd, 0, 0, &image);
+        putImageLcd(&lcd, 0, 0, &image);
 
-		if (ioctl(vfd, VIDIOC_QBUF, &buffer) == -1)
-		{
-			perrorLog(
-				isDaemon,
-				program,
-				"could not enqueue video buffers");
+        if (ioctl(vfd, VIDIOC_QBUF, &buffer) == -1)
+        {
+            perrorLog(
+                isDaemon,
+                program,
+                "could not enqueue video buffers");
 
-			close(vfd);
+            close(vfd);
 
-			if (pfh)
-			{
-				pidfile_remove(pfh);
-			}
+            if (pfh)
+            {
+                pidfile_remove(pfh);
+            }
 
-			exit(EXIT_FAILURE);
-		}
+            exit(EXIT_FAILURE);
+        }
 
         //-----------------------------------------------------------------
 
@@ -763,40 +763,40 @@ main(
 
     //---------------------------------------------------------------------
 
-	destroyImage(&image);
+    destroyImage(&image);
 
     //---------------------------------------------------------------------
 
-	buf_type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    buf_type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
 
-	if (ioctl(vfd, VIDIOC_STREAMOFF, &buf_type) == -1)
-	{
-		perrorLog(
-			isDaemon,
-			program,
-			"could not stop video capture");
+    if (ioctl(vfd, VIDIOC_STREAMOFF, &buf_type) == -1)
+    {
+        perrorLog(
+            isDaemon,
+            program,
+            "could not stop video capture");
 
-		close(vfd);
+        close(vfd);
 
-		if (pfh)
-		{
-			pidfile_remove(pfh);
-		}
+        if (pfh)
+        {
+            pidfile_remove(pfh);
+        }
 
-		exit(EXIT_FAILURE);
-	}
+        exit(EXIT_FAILURE);
+    }
 
     //--------------------------------------------------------------------
 
-	for (bufferIndex = 0 ;
-		 bufferIndex < numberOfVideoBuffers ;
-		 bufferIndex++)
-	{
-		munmap(videoBuffers[bufferIndex].buffer,
-			   videoBuffers[bufferIndex].length);
-	}
+    for (bufferIndex = 0 ;
+         bufferIndex < numberOfVideoBuffers ;
+         bufferIndex++)
+    {
+        munmap(videoBuffers[bufferIndex].buffer,
+               videoBuffers[bufferIndex].length);
+    }
 
-	free(videoBuffers);
+    free(videoBuffers);
 
     //--------------------------------------------------------------------
 
